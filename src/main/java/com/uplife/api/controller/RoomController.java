f6693c088df93bc2387a84832611bdeced4f566c
pida <epidanou@hotmail.fr>
2019-05-14 17:00:15 +0200

    bdd connectée + request ok pour room

--- src/main/java/com/uplife/api/controller/RoomController.java
+++ src/main/java/com/uplife/api/controller/RoomController.java
@@ -4,26 +4,37 @@ import com.uplife.api.exception.RoomNotFoundException;
 import com.uplife.api.model.Room;
 import com.uplife.api.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RoomController {
 
 @Autowired
-/*RoomRepository RoomRepository;
+private RoomRepository RoomRepository;
 
-/*@GetMapping("/rooms")
-    public List<Room> getAllNotes(){
+@GetMapping("/rooms")
+    public List<Room> getAllRooms(){
     return RoomRepository.findAll();
-}*/
-
-@GetMapping("/room")
-    public String test(){
-    return "test";
 }
 
+@GetMapping("/room/getById/{id}")
+    public Optional<Room> getRoomByID(@PathVariable long id){
+    return RoomRepository.findById(id);
+}
+
+@GetMapping("/room/getByName/{name}")
+    public Optional<Room> getRoomByName(@PathVariable String name){
+    return RoomRepository.findRoomByName(name);
+}
+
+@GetMapping("/room/getAllByCampus/{campus}")
+    public List<Room> getRoomsByCampus(@PathVariable String campus){
+    return RoomRepository.findRoomByCampus(campus);
+}
 
 }
--- src/main/java/com/uplife/api/uplife_spring_api/UplifeSpringApiApplication.java
+++ src/main/java/com/uplife/api/uplife_spring_api/UplifeSpringApiApplication.java
@@ -2,8 +2,14 @@ package com.uplife.api.uplife_spring_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.uplife.api.controller")
+@EntityScan("com.uplife.api.model")
+@EnableJpaRepositories("com.uplife.api.repository")
 public class UplifeSpringApiApplication {
 
     public static void main(String[] args) {
